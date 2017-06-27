5d290541c1b7d0fc3bb24ac14f55c0d6c0d3bd5e
GnUfTw <maxpung@gmail.com>
2017-06-27 05:08:28 -0500

    Commented out calendar code leaving this view with a empty fragment. I am having trouble getting the CalendarView to display correctly. When this stuff is not commented out, the fragment takes a really long time load when selected from the action bar and the calendar ends up rendering in a goofy ass way. I am going to address this issue later on.

--- app/src/main/java/com/gnuftw/bestandworst/CalendarFragment.java
+++ app/src/main/java/com/gnuftw/bestandworst/CalendarFragment.java
@@ -1,11 +1,11 @@
 package com.gnuftw.bestandworst;
 
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
 
 public class CalendarFragment extends Fragment {
     @Override
@@ -14,4 +14,10 @@ public class CalendarFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
+
+    /*public void onSelectedDayChange(CalendarView calendarView, int year, int month, int dayOfMonth) {
+        // Need to pull the best & worst entry for the selected date and display it in the
+        // text views below the calendar. If the selected date has no entry, the user should
+        // be notified.
+    }*/
 }
