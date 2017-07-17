97cdea456db5681f1752d90c42e91a8c5c60e22d
GnUfTw <maxpung@gmail.com>
2017-07-17 18:21:44 -0500

    Added information to about fragment including version number, the gnu logo (for fun), an email to reach me, and a link to the projects github page.

--- app/src/main/java/com/gnuftw/bestandworst/AboutFragment.java
+++ app/src/main/java/com/gnuftw/bestandworst/AboutFragment.java
@@ -1,11 +1,15 @@
 package com.gnuftw.bestandworst;
 
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class AboutFragment extends Fragment {
     @Override
@@ -13,5 +17,31 @@ public class AboutFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        TextView versionTextView = (TextView) getActivity().findViewById(R.id.versionTextView);
+        versionTextView.setText(getVersionInfo());
+    }
+
+    private String getVersionInfo() {
+        String strVersion = "Version: ";
+        PackageInfo packageInfo;
+        try {
+            packageInfo = getActivity().getApplicationContext()
+                    .getPackageManager()
+                    .getPackageInfo(
+                            getActivity().getApplicationContext().getPackageName(),
+                            0
+                    );
+            strVersion += packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.d("AboutFragment", "Unknown version number");
+            strVersion += "unknown";
+        }
+
+        return strVersion;
     }
 }
